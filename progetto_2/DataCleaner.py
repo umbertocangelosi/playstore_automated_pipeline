c66fdc2e583c8a38fb14dbef1ed6e9519cac71a0
Alessio Spagnolo <spagnoloalessio88@gmail.com>
2023-03-07 16:39:28 +0100

    umberto vai tranquillo

--- progetto_2/DataCleaner.py
+++ progetto_2/DataCleaner.py
@@ -1,51 +1,73 @@
 # print(df['Size'][df['Size'] != 'Varies with device'][~df['Size'].str.contains('M')[~df['Size'].str.contains('k')])
+import pandas as pd
+def column_to_number2(df,col):
+    df[col] = df[col].str.replace("Varies with device",'')
+    df[col] = df[col].str.replace('M', '000000')
+    df[col] = df[col].str.replace('k','000')
+    df[col] = pd.to_numeric(df[col].str.replace('[^0-9.]', '',regex=True))
+    return df
 
-class DataProcessing:
+class DataCleaner:
     
     def __init__(self):
         pass
     
-    # why this is not working ?
-    def to_number(self, dataframe,column):        
-        # leave only numbers and dots, then cast to int64
-        dataframe = dataframe.assign(column = pd.to_numeric(df[column].str.replace('[^0-9.]', '')))
+    # def to_number(self,dataframe,column):
+        
+    #     # leave only numbers and dots, then cast to int64
+    #     dataframe = dataframe.assign(**{column: pd.to_numeric(dataframe[column].str.replace('[^0-9.]', '', regex=True))})
+    #     return dataframe
+    
+    def column_to_number(df,col):
+        df[col] = df[col].str.replace("Varies with device",'')
+        df[col] = df[col].str.replace('M', '000000')
+        df[col] = df[col].str.replace('k','000')
+        df[col] = pd.to_numeric(df[col].str.replace('[^0-9.]', '',regex=True))
+        return df
+
+    def comma_to_dot(dataframe,column):
+        # replace commas with dots, working so far
+        dataframe[column] = dataframe[column].str.replace(',','.')
+        pass
+
+    # def size_to_number():
+    #     # replace (M with '000',k with '',G with '000000'). output in mega
+    #     pass
+
+    # def convert_date():
+    #     # convert date from string to date
+    #     pass
+    
+    def remove_column_duplicates(dataframe,column):
+        # remove duplicates entries with the same Name, platform, genre
+        dataframe.drop_duplicates(subset=column, inplace=True).reset_index(drop=True)
         return dataframe
 
-    def comma_to_dot():
-        # replace commas with dots
-        pass
-
-    def size_to_number():
-        # replace (M with '000',k with '',G with '000000'). output in mega
-        pass
-
-    # se ha senso si fa anche questa, altrimenti no
-
-    def convert_date():
-        # convert date from string to date
-        pass
-    
-    def remove_duplicates():
-        # remove duplicates entries with the same Name, platform, genre
-        pass
-    
-    def standardize_text(format=('l','t','c')):
+    def standardize_text(dataframe,column):
         # return all entries of a column in string lower case/title/capitalize, based on the parameter format
-        pass
-    
-    def convert_types():
-        # convert data types from x to y
-        pass
+        if dataframe[column].dtype != int:
+            dataframe[column] = dataframe[column].str.lower()
+            return dataframe
+        else:
+            print('Column type is not string\nAborting function')
+            pass
+        
+    # def convert_types():
+    #     # convert data types from x to y
+    #     pass
     
     # Handling missing data
     
-    def remove_na():
+    def remove_na(dataframe,column):
         #remove rows with missing revelant data
-        pass
-    def fill_na():
+        dataframe.dropna(subset=column,inplace=True)
+        return dataframe
+
+    def fill_na(dataframe,column):
+
         # fill empty values with median,mean,mode, whatever
-        pass
-    
-    # drop column?
+        dataframe[column].fillna(value=dataframe[column].median(),inplace=True)
+        return dataframe
+
     
     
