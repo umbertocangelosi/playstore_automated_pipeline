c70139b2650f5d97dff08a472ef32c51ec81fe74
convuz01 <56539492+convuz01@users.noreply.github.com>
2023-03-23 16:03:53 +0100

    modifica file analisi

--- progetto_2/analisi/sprint5/riccardo_convertino5.py
+++ progetto_2/analisi/sprint5/riccardo_convertino5.py
@@ -8,4 +8,17 @@ dc = DataCleaner()
 df = pd.read_csv(r'C:\Users\riccardo\Desktop\corso_programmazione\develhope_2023_team1\progetto_2\database\googleplaystore.csv')
 dc.clean_all(df)
 
-print(df.isna().sum())
+#1-split dataset into 4 parts and pickle them (search for pickle if you don't know what it is)
+df_split = np.array_split(df, 4)
+for i in range(4):
+    df_split[i].to_pickle(R"./rick_pickle/split{i}.pkl")  
+
+#2-unlock the pickling and merge the dataset as follow 2-4-1-3 
+
+
+#3-reverse the whole data set (the last row will be the first one) 
+
+
+#4-apply sliding window technic and store the patches in a 2D list
+
+
