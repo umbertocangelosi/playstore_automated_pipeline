00660d9afed3f597bba3717dd32c9de477e9e815
umbertocangelosi <umbertocangelosi97@outlook.it>
2023-04-08 06:57:08 +0200

    upgrade data ingestor(postgres), upgrade main(postgres)

--- progetto_2/main.py
+++ progetto_2/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import psycopg2
+from sqlalchemy import create_engine
 
 from src.DataIngestor import DataIngestor
 from src.DataCleaner import DataCleaner
@@ -13,22 +15,38 @@ dc = DataCleaner()
 da = DataAnalyser()
 dv = DataVisualizer()
 
-google_data = di.read_file("./progetto_2/data/raw/googleplaystore.csv")
+google_data = di.read_file("./data/raw/googleplaystore.csv")
 google_data = dc.clean_googledb(google_data)
-di.save_file(google_data, "./progetto_2/data/output/googleplaystore_cleaned.pkl")
 
-negative = di.read_file('./progetto_2/data/raw/n.xlsx')
-positive = di.read_file('./progetto_2/data/raw/p.xlsx')
-sentiment_words = dc.clean_sentiment_list(positive,negative)
 
-google_reviews = di.read_file('./progetto_2/data/raw/googleplaystore_user_reviews.csv')
+'''qui ci sono diverse operazioni di cleaning importanti da mettere dentro il 
+datacleaner per potere interagire senza errori con postgres'''
+google_data=google_data.dropna(subset='Type')
+google_data=google_data.rename(columns={'App': 'app','Category':'category','Rating':'rating','Reviews':'reviews','Size':'size','Installs':'installs','Type':'type','Price':'price','Content Rating':'content_rating','Genres':'genres','Last Updated':'last_update'})
+
+google_reviews = di.read_file('./data/raw/googleplaystore_user_reviews.csv')
 google_reviews = dc.clean_googlereviews(google_reviews)
-google_reviews = dc.replace_common_strings(google_reviews, 'Translated_Review', sentiment_words)
-di.save_file(google_reviews, './progetto_2/data/output/google_reviews.pkl')
 
-google_data = da.assign_sentiment(google_data, google_reviews)
-di.save_file(google_data, './progetto_2/data/output/google_scored.pkl')
-print ('\ngoogle_scored.pkl has been stored successfully\n')
-google_sentiment_data = di.read_file('./progetto_2/data/output/google_scored.pkl')
-print('\n\n\nMAIN PROCESS HAS BEEN COMPLETED\n\n\n')
+'''idem con patatate ci sono diverse operazioni di cleaning importanti da mettere dentro il 
+datacleaner per potere interagire senza errori con postgres'''
+google_reviews = google_reviews.rename(columns={'App':'app','Translated_Review':'translated_review'})
+google_reviews=google_reviews.dropna()
+google_reviews['translated_review'] = google_reviews['translated_review'].astype(str)
+google_reviews=google_reviews[google_reviews['app'].isin(google_data['app'])]
 
+#ora i dati dovrebbero essere pronti per la parte di caricamento nel database
+
+#creo la connessione al database con psycopg2
+conn = di.connect('postgres','postgres','almno','localhost','5432')
+
+#creo le tabelle vuote tramite psycopg passando internamente le ddl
+di.create_google(conn)
+di.create_reviews(conn)
+
+#creo una connessione al database compatibile con il metodo db.to_sql(), usando la libreria sqlalchemy
+engine=di.create_engine('postgres','almno','localhost','5432','postgres')
+
+
+#carico i dati dei dataframe dentro postgress, che fungera' ora da data warehouseb
+di.load(google_data,'googleplaystore',engine,replace=True,conn=conn)
+di.load(google_reviews,'google_reviews',engine,replace=True,conn=conn)
--- progetto_2/src/DataIngestor.py
+++ progetto_2/src/DataIngestor.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
 class DataIngestor():
     
@@ -41,3 +43,108 @@ class DataIngestor():
             return data.to_html(filepath, index=False)
         elif ext == '.pkl':
             return data.to_pickle(filepath)
+    '''la funzione crea un collegamento diretto con il database postgresql tramite la libreria psycopg2
+    passandogli tutti i parametri, sicuramente sara conveniente assegnare il risultato a una variabile conn=di.connect()'''
+    
+    def connect(self,dbname,dbuser,dbpass,dbhost,dbport):
+        DB_NAME = dbname
+        DB_USER = dbuser
+        DB_PASS = dbpass
+        DB_HOST = dbhost
+        DB_PORT = dbport
+
+        try:
+            conn = psycopg2.connect(database=DB_NAME,
+                                    user=DB_USER,
+                                    password=DB_PASS,
+                                    host=DB_HOST,
+                                    port=DB_PORT)
+            print("Database connected successfully")
+            print('Cursor object created')
+            return conn
+        except:
+            print("Database not connected successfully")  
+    
+
+    
+    #la funzione crea il database vuoto su postgres sfruttando il metodo .cursor() della classe conn, ossia la classe delle connessioni 
+    #in psycopg2, tramite il metodo possiamo poi eseguire con execute() una query ddl per creare il database, ho assegnato come chiave primaria
+    #il nome delle App. e' necessario fare commit dei cambiamenti effettuati nel rispetivo cursore.
+    
+    def create_google(self,conn):
+        cur =conn.cursor()
+        # executing queries to create table
+        cur.execute("""
+        DROP TABLE IF EXISTS googleplaystore CASCADE;
+
+            CREATE TABLE IF NOT EXISTS googleplaystore
+        (
+            App TEXT PRIMARY KEY NOT NULL,
+            Category TEXT NOT NULL,
+            Rating INTEGER NOT NULL,
+            Reviews INTEGER NOT NULL,
+            Size INTEGER NOT NULL,
+            Installs INTEGER NOT NULL,
+            Type TEXT NOT NULL,
+            Price FLOAT NOT NULL,
+            Content_Rating TEXT NOT NULL,
+            Genres TEXT NOT NULL,
+            Last_Update TEXT NOT NULL
+        )
+                    """)
+            #commit the changes
+        conn.commit()
+        print("Table Created successfully")
+    
+    '''qui ho creato una colonna con l'id seriale della recenzione, e ho creato il collegamento tra app di reviews e app di google'''
+    def create_reviews(self,conn):
+        cur =conn.cursor()
+
+        # executing queries to create table
+        cur.execute("""
+        DROP TABLE IF EXISTS google_reviews CASCADE;
+
+            CREATE TABLE IF NOT EXISTS google_reviews
+        (   
+            id SERIAL PRIMARY KEY,
+            APP TEXT NOT NULL REFERENCES googleplaystore(APP),
+            TRANSLATED_REVIEW TEXT NOT NULL 
+
+
+        )
+                    """)
+
+        #commit the changes
+        conn.commit()
+        print("Table Created successfully")
+
+
+    #qui sono obbligato a creare un'altra connessione come quella di psycopg2, ma usando sqlalchemy, perche' il metodo pd.to_sql() e' compatibile
+    #con sqlalchemy e non con psycopg2
+    def create_engine(self,username,password,host,port,database):
+        try:
+            engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}') 
+            print("Created engine successfully")
+            
+            return engine
+        
+        except:
+            print("Create engine failure")  
+           
+
+    #funzione che  carica il dataframe nella sua tabella, entrambi parametri da passare e che obbligatoriamente devono essere compatibili,
+    #sono stato costretto a mettere quel parametro replace, perche' quando si caricano dei nuovi dati si deve poter scegliere di eliminare i dati precedenti
+    #e inserire i nuovi, si potrebbe fare anche scrivendo replace al posto di 'append', ma dato che abbiamo un collegamento tra reviews e google in app,
+    #non permette di rimpiazzare i dati e da errore tramite quel parametro
+    def load(self,dataframe,tabella,engine,replace=False,conn=None):  
+
+        if replace==True:
+            cur=conn.cursor()
+            cur.execute("""
+        DROP TABLE IF EXISTS tabella CASCADE;
+        """)
+
+        
+        dataframe.to_sql(tabella, con=engine, if_exists='append', index=False)
+        print('Load to postgres success')
+       
