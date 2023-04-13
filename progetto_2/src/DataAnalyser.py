d98292c1402c44b376bc5727555eb726c5ed3110
Alessio Spagnolo <spagnoloalessio88@gmail.com>
2023-04-13 14:20:44 +0200

    internal fix

--- progetto_2/src/DataAnalyser.py
+++ progetto_2/src/DataAnalyser.py
@@ -13,9 +13,9 @@ class DataAnalyser:
     def assign_sentiment (self, dataframe, data_reviews):
         dataframe.name = 'Google Database'
         print(f'\nAssigning sentiment to {dataframe.name}...\n')
-        data_reviews['Score'] = data_reviews['Translated_Review'].apply(afn.score)
-        app_score = data_reviews.groupby(by='App').agg({'Score':'mean'}).reset_index()
-        dataframe = pd.merge(dataframe, app_score, how='left', on='App')
+        data_reviews['score'] = data_reviews['translated_review'].apply(afn.score)
+        app_score = data_reviews.groupby(by='app').agg({'score':'mean'}).reset_index()
+        dataframe = pd.merge(dataframe, app_score, how='left', on='app')
         dataframe.name = 'Google Database'
         print(f'\nSentiment has been assigned to {dataframe.name}!\n')
         return dataframe
--- progetto_2/src/DataIngestor.py
+++ progetto_2/src/DataIngestor.py
@@ -22,7 +22,7 @@ class DataIngestor():
         elif ext == 'pkl':
             return pd.read_pickle(filepath)  
         else:
-            return 'function not implemented for this type of file yet'
+            return '\nfunction not implemented for this type of file yet\n'
     
     def save_file (self, data, filepath):
         ext = filepath.split('.')[-1]
@@ -36,111 +36,25 @@ class DataIngestor():
             return data.to_html(filepath, index=False)
         elif ext == 'pkl':
             return data.to_pickle(filepath)
-        
-    '''la funzione crea un collegamento diretto con il database postgresql tramite la libreria psycopg2
-    passandogli tutti i parametri, sicuramente sara conveniente assegnare il risultato a una variabile conn=di.connect()'''
     
-    def pg2_connect(self,dbname,dbuser,dbhost,dbport):
-
-        dbpass = input (f"\nInsert {dbname} password for {dbuser} user: ")
-
-        try:
-            conn = psycopg2.connect(database=dbname,
-                                    user=dbuser,
-                                    password=dbpass,
-                                    host=dbhost,
-                                    port=dbport)
-            print("\nConnection established")
-            print("\nCursor object created")
-            return conn
-        
-        except:
-            print("\nConnection failed\n")  
-    
-
-    
-    #la funzione crea il database vuoto su postgres sfruttando il metodo .cursor() della classe conn, ossia la classe delle connessioni 
-    #in psycopg2, tramite il metodo possiamo poi eseguire con execute() una query ddl per creare il database, ho assegnato come chiave primaria
-    #il nome delle App. e' necessario fare commit dei cambiamenti effettuati nel rispettivo cursore.
-    
-    def create_table_google(self, conn):
-
-        cur = conn.cursor()
-        
-        cur.execute("""
-        DROP TABLE IF EXISTS google_play_store CASCADE;
-
-            CREATE TABLE IF NOT EXISTS google_play_store
-        (
-            app TEXT PRIMARY KEY NOT NULL,
-            category TEXT NOT NULL,
-            rating FLOAT NOT NULL,
-            reviews INTEGER NOT NULL,
-            size INTEGER NOT NULL,
-            installs INTEGER NOT NULL,
-            type TEXT NOT NULL,
-            price FLOAT NOT NULL,
-            content_rating TEXT NOT NULL,
-            genres TEXT NOT NULL,
-            last_updated TEXT NOT NULL
-        )
-                    """)
-
-        conn.commit()
-
-        print("\nTable google_play_store created!")
-    
-    '''qui ho creato una colonna con l'id seriale della recensione, e ho creato il collegamento tra app di reviews e app di google'''
-    def create_table_reviews(self,conn):
-        cur = conn.cursor()
-
-        # executing queries to create table
-        cur.execute("""
-        DROP TABLE IF EXISTS google_reviews CASCADE;
-
-            CREATE TABLE IF NOT EXISTS google_reviews
-        (   
-            id SERIAL PRIMARY KEY,
-            app TEXT NOT NULL REFERENCES google_play_store(app),
-            translated_review TEXT NOT NULL 
-
-
-        )
-                    """)
-
-        #commit the changes
-        conn.commit()
-        print("Table google_reviews created!")
-
-# dbname,dbuser,dbhost,dbport
-# qui sono obbligato a creare un'altra connessione come quella di psycopg2, ma usando sqlalchemy, perche' il metodo pd.to_sql() e' compatibile
-# con sqlalchemy e non con psycopg2
     def sqlalchemy_connect(self,name,user,host,port):
-
-        my_pass = input (f'Insert {name} password for {user} user: ')
-
+        my_pass = input (f'\nInsert {name} password for {user} user: ')
         try:
             engine = create_engine(f'postgresql://{user}:{my_pass}@{host}:{port}/{name}') 
-            print("Connection sqlalchemy")
+            print("\nConnection sqlalchemy\n")
             return engine
         except:
-            print("Error somewhere")  
-           
-
-#funzione che carica il dataframe nella sua tabella, entrambi parametri da passare e che obbligatoriamente devono essere compatibili,
-#sono stato costretto a mettere quel parametro replace, perche' quando si caricano dei nuovi dati si deve poter scegliere di eliminare i dati precedenti
-#e inserire i nuovi, si potrebbe fare anche scrivendo replace al posto di 'append', ma dato che abbiamo un collegamento tra reviews e google in app,
-#non permette di rimpiazzare i dati e da errore tramite quel parametro
+            print("\nError somewhere\n")  
     
-    def export_to_sql(self, dataframe, name, con, replace=False, pg2=None):
+    def to_cloud(self, dataframe, name, con, if_exists='fail', index=False):
+        dataframe.to_sql(name=name, con=con, if_exists=if_exists, index=index)
+        print(f"\n{dataframe} has been successfully uploaded!\n")
 
-        if replace:
-            cur = pg2.cursor()
-            cur.execute("""
-        DROP TABLE IF EXISTS dataframe CASCADE;
-        """)
+    def from_cloud(self, table):
 
-        
-        dataframe.to_sql(name=name, con=con, if_exists='append', index=False)
-        print("Load to postgres success")
-       
+        url = 'postgresql://onyhtqzn:ej-TLeomNZACBDKE7_PhUfZmCSUcFRW1@surus.db.elephantsql.com/onyhtqzn'
+        alchemy_engine = create_engine(url=url)
+        connection = alchemy_engine.connect()
+        table = pd.read_sql(f'select * from "{table}"', connection)
+        print(f'{table} has been loaded from our database online')
+        return table
